3b11495439bda404a658c85eea07552027da86e3
Murilosantana7 <murilo.santana@shopee.com>
2025-10-16 15:16:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,118 @@
-from playwright.sync_api import sync_playwright
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import time
-import datetime
-import os
-import pytz
 import asyncio
+from playwright.async_api import async_playwright
+from datetime import datetime, timedelta
+import time
+import os
+import shutil
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 
-timezone = pytz.timezone('America/Sao_Paulo')
+DOWNLOAD_DIR = "/tmp"
 
-# Diretório de download para GitHub Actions
-download_dir = "/tmp"
-os.makedirs(download_dir, exist_ok=True)
-
-def login(page):
-    page.goto("https://spx.shopee.com.br/")
-    page.wait_for_selector('xpath=//*[@placeholder="Ops ID"]', timeout=15000)
-    page.fill('xpath=//*[@placeholder="Ops ID"]', 'Ops105579')
-    page.fill('xpath=//*[@placeholder="Senha"]', '@Shopee123')
-    page.click('xpath=/html/body/div[1]/div/div[2]/div/div/div[1]/div[3]/form/div/div/button')
-
-    page.wait_for_timeout(15000)
+def rename_downloaded_file(download_dir, download_path):
     try:
-        page.click('css=.ssc-dialog-close', timeout=5000)
-    except:
-        print("Nenhum pop-up foi encontrado.")
-        page.keyboard.press("Escape")
-
-def get_data(page):
-    data = []
-    try:
-        d1 = "SoC_SP_Louveira"
-        d2 = "SOC_Received"
-
-        # Acessa a página
-        page.goto("https://spx.shopee.com.br/#/orderTracking", timeout=60000)
-
-        # Preenche o primeiro campo
-        time.sleep(5)
-        input1 = page.locator('xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[6]/form[1]/div[8]/div[1]/span[1]/span[1]/div[1]/div[1]/div[1]/span[1]/input[1]')
-        input1.click()
-        input1.fill(d1)
-        time.sleep(5)
-        
-        page.locator('xpath=/html[1]/body[1]/span[6]/div[1]/div[1]/div[1]/ul[1]/div[1]/div[1]/li[1]').click()
-       
-        page.wait_for_timeout(2000)
-        page.locator('xpath=/html/body/div[1]/div/div[2]/div[1]/div[1]/span[2]/span[1]/span').click()
-
-        time.sleep(5)
-        # Preenche o segundo campo
-        input2 = page.locator('xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[6]/form[1]/div[21]/div[1]/span[1]/span[1]/div[1]/div[1]/div[1]/span[1]/input[1]')
-        input2.click()
-        input2.fill(d2)
-        time.sleep(5)
-
-        page.locator('xpath=/html[1]/body[1]/span[4]/div[1]/div[1]/div[1]/ul[1]/li[1]/span[1]/b[1]').click()
-        time.sleep(5)
-
-        page.locator('xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[2]/span[1]/span[1]').click()
-
-        # Clica no botão de pesquisa
-        page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[6]/form/div[25]/button[1]').click()
-        page.wait_for_timeout(10000)
-
-        # Coleta o dado
-        first_value = page.inner_text('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[10]/div/div[3]/div/span[1]')
-        data.append(first_value)
+        current_hour = datetime.now().strftime("%H")
+        new_file_name = f"QUEUE-{current_hour}.csv"
+        new_file_path = os.path.join(download_dir, new_file_name)
+        if os.path.exists(new_file_path):
+            os.remove(new_file_path)
+        shutil.move(download_path, new_file_path)
+        print(f"Arquivo salvo como: {new_file_path}")
+        return new_file_path
     except Exception as e:
-        print(f"Erro ao coletar dados: {e}")
-        raise
+        print(f"Erro ao renomear o arquivo: {e}")
+        return None
 
-    return data
+def update_packing_google_sheets(csv_file_path):
+    try:
+        if not os.path.exists(csv_file_path):
+            print(f"Arquivo {csv_file_path} não encontrado.")
+            return
+        scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+        creds = ServiceAccountCredentials.from_json_keyfile_name("hxh.json", scope)
+        client = gspread.authorize(creds)
+        sheet1 = client.open_by_url("https://docs.google.com/spreadsheets/d/1qvgVViwnLVkzLnjfWQLU3m6ce0f3lXrvg-aq2YF59v8")
+        worksheet1 = sheet1.worksheet("queuelistlog")
+        df = pd.read_csv(csv_file_path)
+        df = df.fillna("")
+        worksheet1.clear()
+        worksheet1.update([df.columns.values.tolist()] + df.values.tolist())
+        print(f"Arquivo enviado com sucesso para a aba 'EXP'.")
+        time.sleep(5)
+    except Exception as e:
+        print(f"Erro durante o processo: {e}")
 
-def update_google_sheets(first_value):
-    scope = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("hxh.json", scope)
-    client = gspread.authorize(creds)
-
-    sheet = client.open_by_url(
-        'https://docs.google.com/spreadsheets/d/1jalr3aWw6HE2sBmDrAYVXL2eM5sjm9pBu6fP5xM6YUk/edit?gid=0#gid=0'
-    ).worksheet("Base")
-
-    sheet.update('BF2', [[first_value]])
-
-def main():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True, args=["--no-sandbox", "--disable-dev-shm-usage"])
-        context = browser.new_context(accept_downloads=True)
-        page = context.new_page()
-
+async def main():        
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False, args=["--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080"])
+        context = await browser.new_context(accept_downloads=True)
+        page = await context.new_page()
         try:
-            login(page)
-            data = get_data(page)
-            if data:
-                update_google_sheets(data[0])
+            # LOGIN
+            await page.goto("https://spx.shopee.com.br/")
+            await page.wait_for_selector('xpath=//*[@placeholder="Ops ID"]', timeout=15000)
+            await page.locator('xpath=//*[@placeholder="Ops ID"]').fill('Ops50020')
+            await page.locator('xpath=//*[@placeholder="Senha"]').fill('@Shopee123')
+            await page.locator('xpath=/html/body/div[1]/div/div[2]/div/div/div[1]/div[3]/form/div/div/button').click()
+            await page.wait_for_timeout(15000)
+            try:
+                await page.locator('.ssc-dialog-close').click(timeout=5000)
+            except:
+                print("Nenhum pop-up foi encontrado.")
+                await page.keyboard.press("Escape")
+
+            # NAVEGAÇÃO E DOWNLOAD
+            await page.goto("https://spx.shopee.com.br/#/queue-list")
+            await page.wait_for_timeout(10000)
+            await page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/div/div[2]/button[2]/span').click()
+            await page.wait_for_timeout(10000)
+            await page.locator('xpath=/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/div[2]/button/span').click()
+            await page.wait_for_timeout(10000)
+            
+            d3 = (datetime.now() - timedelta(days=7)).strftime("%Y/%m/%d")
+            d1 = (datetime.now() + timedelta(days=1)).strftime("%Y/%m/%d")
+
+            # Primeiro campo de data
+            date_input = page.get_by_role("textbox", name="Data de início").nth(1)
+            await date_input.wait_for(state="visible", timeout=10000)
+            await date_input.click(force=True)
+            await date_input.fill(d3)
+
+            # Segundo campo de data
+            date_input = page.get_by_role("textbox", name="Data final").nth(1)
+            await date_input.wait_for(state="visible", timeout=10000)
+            await date_input.click(force=True)
+            await date_input.fill(d1)
+            await page.wait_for_timeout(5000)
+            
+
+            await page.get_by_text("Exportar").click()
+            await page.wait_for_timeout(5000)
+            
+            await page.get_by_role('button', name='Confirm').click()
+            await page.wait_for_timeout(15000)
+
+          
+            # 👉 Botão de download
+            async with page.expect_download() as download_info:
+                await page.get_by_role("button", name="Baixar").nth(0).click()
+                #await page.locator('xpath=/html/body/span/div/div[1]/div/span/div/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/button').click()
+            download = await download_info.value
+            download_path = os.path.join(DOWNLOAD_DIR, download.suggested_filename)
+            await download.save_as(download_path)
+            new_file_path = rename_downloaded_file(DOWNLOAD_DIR, download_path)
+            
+
+            # Atualizar Google Sheets (opcional)
+            if new_file_path:
+                update_packing_google_sheets(new_file_path)
                 print("Dados atualizados com sucesso.")
-            else:
-                print("Nenhum dado encontrado.")
         except Exception as e:
             print(f"Erro durante o processo: {e}")
         finally:
-            browser.close()
+            await browser.close()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
